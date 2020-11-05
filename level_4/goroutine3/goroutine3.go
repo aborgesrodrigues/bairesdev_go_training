f71588db8cd2e00cf49ba1f730c55817abe93868
Alessandro Borges Rodrigues <aborgesrodrigues@gmail.com>
2020-11-05 16:41:05 -0300

    changed main function of gorotine3.go

--- level_4/goroutine3/goroutine3.go
+++ level_4/goroutine3/goroutine3.go
@@ -12,7 +12,14 @@ func main() {
 	ch := make(chan int)
 
 	go goroutine(10, ch)
-	go goroutine(100, ch)
+	go goroutine(20, ch)
+	go goroutine(30, ch)
+	go goroutine(40, ch)
+	go goroutine(50, ch)
+
+	fmt.Println("Channel", <-ch)
+	fmt.Println("Channel", <-ch)
+	fmt.Println("Channel", <-ch)
 	fmt.Println("Channel", <-ch)
 	fmt.Println("Channel", <-ch)
 }
