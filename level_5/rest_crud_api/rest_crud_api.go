00e7e6fcc7ce4d355e37d3b5fc2f72dda09cf340
Alessandro Borges Rodrigues <aborgesrodrigues@gmail.com>
2020-11-09 13:00:41 -0300

    testcases included

--- level_5/rest_crud_api/rest_crud_api.go
+++ level_5/rest_crud_api/rest_crud_api.go
@@ -6,40 +6,38 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
 
 type product struct {
-	ID    string  `json:"ID"`
-	Code  string  `json:"Code"`
-	Name  string  `json:"Name"`
-	Price float64 `json:"Price"`
+	ID    int     `json:"id"`
+	Code  string  `json:"code"`
+	Name  string  `json:"name"`
+	Price float64 `json:"price"`
 }
 
 type productInventory struct {
-	Product  product `json:"Product"`
-	Quantity int     `json:"Quantity"`
+	Product  product `json:"product"`
+	Quantity int     `json:"quantity"`
+}
+
+type productInventoryPatch struct {
+	Product  *product `json:"product,omitempty"`
+	Quantity *int     `json:"quantity"`
 }
 
 var inventory []productInventory
-
-// func findProductIndex(product product) int {
-// 	for i, productInventory := range inventory {
-// 		if productInventory.Product.ID == product.ID {
-// 			return i
-// 		}
-// 	}
-// 	return -1
-// }
 
 func createProduct(w http.ResponseWriter, r *http.Request) {
 	var newInventory productInventory
 
 	reqBody, err := ioutil.ReadAll(r.Body)
-	fmt.Println(err)
 	if err != nil {
+		//json.NewEncoder(w).Encode("Kindly enter data with the Product title and description only in order to update")
 		fmt.Fprintf(w, "Kindly enter data with the Product title and description only in order to update")
+		return
 	}
 
 	json.Unmarshal(reqBody, &newInventory)
@@ -53,7 +51,8 @@ func getOneProduct(w http.ResponseWriter, r *http.Request) {
 	ProductID := mux.Vars(r)["id"]
 
 	for _, singleInventoryProduct := range inventory {
-		if singleInventoryProduct.Product.ID == ProductID {
+		intProductID, _ := strconv.ParseInt(ProductID, 10, 64)
+		if singleInventoryProduct.Product.ID == int(intProductID) {
 			json.NewEncoder(w).Encode(singleInventoryProduct)
 			break
 		}
@@ -71,11 +70,41 @@ func updateProduct(w http.ResponseWriter, r *http.Request) {
 	reqBody, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		fmt.Fprintf(w, "Kindly enter data with the Product title and description only in order to update")
+		return
+	}
+	json.Unmarshal(reqBody, &updatedInventory)
+
+	intProductID, _ := strconv.Atoi(ProductID)
+
+	if updatedInventory.Product.ID != intProductID {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(updatedInventory)
+		return
+	}
+
+	for i, singleInventoryProduct := range inventory {
+		if singleInventoryProduct.Product.ID == intProductID {
+			inventory[i] = updatedInventory
+			json.NewEncoder(w).Encode(singleInventoryProduct)
+			break
+		}
+	}
+}
+
+func updateProductPatch(w http.ResponseWriter, r *http.Request) {
+	ProductID := mux.Vars(r)["id"]
+	var updatedInventory productInventory
+
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Fprintf(w, "Kindly enter data with the Product title and description only in order to update")
+		return
 	}
 	json.Unmarshal(reqBody, &updatedInventory)
 
 	for i, singleInventoryProduct := range inventory {
-		if singleInventoryProduct.Product.ID == ProductID {
+		intProductID, _ := strconv.Atoi(ProductID) //strconv.ParseInt(ProductID, 10, 64)
+		if singleInventoryProduct.Product.ID == int(intProductID) {
 
 			//Check if the attributes of the product was changed
 			if updatedInventory.Product.Code != "" {
@@ -90,7 +119,7 @@ func updateProduct(w http.ResponseWriter, r *http.Request) {
 			if updatedInventory.Quantity != 0 {
 				singleInventoryProduct.Quantity = updatedInventory.Quantity
 			}
-			inventory = append(inventory[:i], singleInventoryProduct)
+			inventory[i] = singleInventoryProduct
 			json.NewEncoder(w).Encode(singleInventoryProduct)
 			break
 		}
@@ -101,7 +130,8 @@ func deleteProduct(w http.ResponseWriter, r *http.Request) {
 	ProductID := mux.Vars(r)["id"]
 
 	for i, singleInventoryProduct := range inventory {
-		if singleInventoryProduct.Product.ID == ProductID {
+		intProductID, _ := strconv.ParseInt(ProductID, 10, 64)
+		if singleInventoryProduct.Product.ID == int(intProductID) {
 			inventory = append(inventory[:i], inventory[i+1:]...)
 			fmt.Fprintf(w, "The Product with ID %v has been deleted successfully", ProductID)
 			break
@@ -114,7 +144,8 @@ func main() {
 	router.HandleFunc("/inventory", createProduct).Methods("POST")
 	router.HandleFunc("/inventory", getAllProducts).Methods("GET")
 	router.HandleFunc("/inventory/{id}", getOneProduct).Methods("GET")
-	router.HandleFunc("/inventory/{id}", updateProduct).Methods("PATCH")
+	//router.HandleFunc("/inventory/{id}", updateProductPatch).Methods("PATCH")
+	router.HandleFunc("/inventory/{id}", updateProduct).Methods("PUT")
 	router.HandleFunc("/inventory/{id}", deleteProduct).Methods("DELETE")
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
---
+++ level_5/rest_crud_api/rest_crud_api_test.go
@@ -0,0 +1,202 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/gorilla/mux"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateProduct(t *testing.T) {
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/inventory/", createProduct).Methods("POST")
+
+	ts := httptest.NewServer(router)
+	defer ts.Close()
+
+	payload := productInventory{
+		Product: product{
+			ID:    1,
+			Code:  "Code 1",
+			Name:  "Name 1",
+			Price: 15.42,
+		},
+		Quantity: 1,
+	}
+
+	data, err := json.Marshal(payload)
+
+	if err != nil {
+		fmt.Println(err)
+
+	}
+	res, err := http.Post(ts.URL+"/inventory/", "application/json", strings.NewReader(string(data)))
+	defer res.Body.Close()
+
+	//fmt.Println(inventory, len(inventory), payload, strings.NewReader(payload))
+	assert.Nil(t, err)
+	assert.Equal(t, res.StatusCode, http.StatusCreated, "they should be equal")
+	assert.Equal(t, len(inventory), 1, "they should be equal")
+	assert.Equal(t, inventory[0].Product.ID, 1, "they should be equal")
+
+}
+
+func TestGetOneProduct(t *testing.T) {
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/inventory/{id}", getOneProduct).Methods("GET")
+
+	ts := httptest.NewServer(router)
+	defer ts.Close()
+
+	fillInventory()
+
+	res, err := http.Get(ts.URL + "/inventory/1")
+	defer res.Body.Close()
+
+	var newInventory productInventory
+	resBody, err := ioutil.ReadAll(res.Body)
+	json.Unmarshal(resBody, &newInventory)
+
+	assert.Nil(t, err)
+	assert.Equal(t, res.StatusCode, http.StatusOK, "they should be equal")
+	assert.Equal(t, newInventory, inventory[0], "they should be equal")
+
+}
+
+func fillInventory() {
+	inventory = []productInventory{
+		{
+			Product: product{
+				ID:    1,
+				Code:  "Code 1",
+				Name:  "Name 1",
+				Price: 15.42,
+			},
+			Quantity: 1,
+		},
+		{
+			Product: product{
+				ID:    2,
+				Code:  "Code 2",
+				Name:  "Name 2",
+				Price: 1225.55,
+			},
+			Quantity: 2,
+		},
+	}
+}
+
+func TestGetAllProducts(t *testing.T) {
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/inventory", getAllProducts).Methods("GET")
+
+	fillInventory()
+
+	ts := httptest.NewServer(router)
+	defer ts.Close()
+
+	res, err := http.Get(ts.URL + "/inventory")
+	defer res.Body.Close()
+
+	var newInventory []productInventory
+	resBody, err := ioutil.ReadAll(res.Body)
+	json.Unmarshal(resBody, &newInventory)
+
+	assert.Nil(t, err)
+	assert.Equal(t, res.StatusCode, http.StatusOK, "they should be equal")
+	assert.Equal(t, newInventory, inventory, "they should be equal")
+
+}
+
+func TestDeleteProduct(t *testing.T) {
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/inventory/{id}", deleteProduct).Methods("DELETE")
+
+	ts := httptest.NewServer(router)
+	defer ts.Close()
+
+	req := httptest.NewRequest("DELETE", ts.URL+"/inventory/1/", nil)
+
+	//q := req.URL.Query()
+	//q.Add("id", "1")
+	//req.URL.RawQuery = q.Encode()
+
+	res := httptest.NewRecorder()
+	handler := http.HandlerFunc(deleteProduct)
+
+	handler.ServeHTTP(res, req)
+
+	fillInventory()
+
+	//res, err := http.Get(ts.URL + "/inventory/1")
+	//defer res.Body.Close()
+
+	var newInventory productInventory
+	var oldInventory productInventory = inventory[0]
+	resBody, err := ioutil.ReadAll(res.Body)
+	json.Unmarshal(resBody, &newInventory)
+
+	assert.Nil(t, err)
+	assert.Equal(t, res.Code, http.StatusOK, "they should be equal")
+	assert.Equal(t, newInventory, oldInventory, "they should be equal")
+	assert.Equal(t, len(inventory), 1, "they should be equal")
+
+}
+
+func TestUpdateProduct(t *testing.T) {
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/inventory/{id}", updateProduct).Methods("PUT")
+
+	ts := httptest.NewServer(router)
+	defer ts.Close()
+
+	payload := productInventory{
+		Product: product{
+			ID:    1,
+			Code:  "Code 1",
+			Name:  "Name 1",
+			Price: 15.42,
+		},
+		Quantity: 1,
+	}
+
+	data, err := json.Marshal(payload)
+
+	if err != nil {
+		fmt.Println(err)
+
+	}
+
+	req := httptest.NewRequest("PUT", ts.URL+"/inventory/1", strings.NewReader(string(data)))
+
+	//q := req.URL.Query()
+	//q.Add("id", "1")
+	//req.URL.RawQuery = q.Encode()
+
+	res := httptest.NewRecorder()
+	handler := http.HandlerFunc(updateProduct)
+
+	handler.ServeHTTP(res, req)
+
+	fillInventory()
+
+	//res, err := http.Get(ts.URL + "/inventory/1")
+	//defer res.Body.Close()
+
+	var newInventory productInventory
+	var oldInventory productInventory = inventory[0]
+	resBody, err := ioutil.ReadAll(res.Body)
+	json.Unmarshal(resBody, &newInventory)
+
+	assert.Nil(t, err)
+	assert.Equal(t, res.Code, http.StatusOK, "they should be equal")
+	assert.Equal(t, newInventory, oldInventory, "they should be equal")
+	assert.Equal(t, len(inventory), 1, "they should be equal")
+
+}
